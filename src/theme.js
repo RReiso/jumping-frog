b9ab31c720d11292f08c4c8173f778896092ed88
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-07 12:52:48 -0500

    Add theme.js

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#9ccc65",
+    },
+    secondary: {
+      main: "#c14834",
+    },
+  },
+});
+
+export default theme;
