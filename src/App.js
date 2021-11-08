8840f1fc6310b3dbe3ee70732d1497612c83734b
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-08 18:02:48 -0500

    Fetch products from commerce.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { commerce } from "./lib/commerce";
 import theme from "./theme";
 import Products from "./components/AllProducts";
 import Navbar from "./components/Navbar/Navbar";
+import useEnhancedEffect from "@mui/utils/useEnhancedEffect";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await commerce.products.list();
+    const { data } = response;
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <ThemeProvider theme={theme}>
       {/* <CssBaseline /> */}
