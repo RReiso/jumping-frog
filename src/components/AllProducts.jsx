575e5c25571e692084c1309de77866f9c285e794
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-09 13:00:25 -0500

    Add opening sentence

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Grid } from "@mui/material";
 import Product from "./Product/Product";
 import "./all-products-styles.css";
+import { Typography } from "@mui/material";
 
 const AllProducts = ({ products }) => {
   return (
     <main>
-      <Grid container justifyContent="center" spacing={1} sx={{ mt: 10 }}>
+      <Typography variant="h6" sx={{ mt: 10, mb: 2, textAlign: "center" }}>
+        Yüksek kaliteli oyuncak seçeneklerimize göz atıniz!
+      </Typography>
+      <Grid container justifyContent="center" spacing={1}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} />
