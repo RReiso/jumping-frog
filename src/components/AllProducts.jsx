fb6bcd0f72d5d3eafee5d060616fc46cbec7390a
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-09 12:24:04 -0500

    Style product card

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -6,7 +6,7 @@ import "./all-products-styles.css";
 const AllProducts = ({ products }) => {
   return (
     <main>
-      <Grid container justifyContent="center" spacing={1} sx={{ mt: 3 }}>
+      <Grid container justifyContent="center" spacing={1} sx={{ mt: 10 }}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <Product product={product} />
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -11,18 +11,17 @@ import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import "./product-styles.css";
 
 const Product = ({ product }) => {
-  console.log(product);
-  // return <div>test</div>;
   return (
     <Card sx={{ mx: { xs: 1, sm: 2 } }}>
       <CardMedia
+        className="image"
         sx={{ m: 1 }}
         image={product.image.url}
         component="img"
         alt={product.name}
       />
-      <CardContent sx={{ bgcolor: "primary.light" }}>
-        <div className="card-main">
+      <CardContent className="card-content" sx={{ bgcolor: "primary.light" }}>
+        <div className="card-info">
           <Typography variant="h6" gutterBottom>
             {product.name}
           </Typography>
