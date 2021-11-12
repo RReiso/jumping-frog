ea4d409d21a04ec96225575b23ec7aa449bd6f3c
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-11 19:18:58 -0500

    Create setup for Checkout component

--- src/components/CheckoutForm/Checkout.jsx
+++ src/components/CheckoutForm/Checkout.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./checkout-styles.css";
+import {
+  Paper,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  CircularProgress,
+  Divider,
+  Button,
+} from "@mui/material";
+
+const steps = ["Shipping address", "Payment details"];
 
 const Checkout = () => {
-  return <div></div>;
+  const [currentStep, setCurrentStep] = useState(0);
+  return (
+    <>
+      <main>
+        <Paper sx={{ mt: 10 }}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={currentStep}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Paper>
+      </main>
+    </>
+  );
 };
 
 export default Checkout;
